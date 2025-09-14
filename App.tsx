0dd26c06d2d2ab11bb5cd61eccb0b627a7ee5f96
JSCode <105148411+jscode-es@users.noreply.github.com>
2025-09-14 13:25:38 +0200

    feat: configuración tailwind

--- App.tsx
+++ App.tsx
@@ -1,45 +1,31 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import { StatusBar, Text, useColorScheme, View } from 'react-native';
+import Config from 'react-native-config';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import './global.css';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+	const isDarkMode = useColorScheme() === 'dark';
 
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
+	return (
+		<SafeAreaProvider>
+			<StatusBar
+				barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+			/>
+			<AppContent />
+		</SafeAreaProvider>
+	);
 }
 
 function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
+	console.log('Environment:', Config);
+	return (
+		<View className="flex-1 items-center justify-center bg-white">
+			<Text className="text-xl font-bold text-blue-500">
+				Welcome to Nativewind!
+				{`\nEnvironment: ${Config.APP_ENV}`}
+			</Text>
+		</View>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
